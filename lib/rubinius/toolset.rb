de62f656b1aaacf8fea7f96bb4abfee336fc27d8
Brian Shirai <brixen@gmail.com>
2013-07-30 15:50:09 -0700

    Added basic methods.

--- lib/rubinius/toolset.rb
+++ lib/rubinius/toolset.rb
@@ -1,7 +1,27 @@
 require "rubinius/toolset/version"
 
 module Rubinius
-  module Toolset
-    # Your code goes here...
+  module ToolSet
+    def self.current
+      @current ||= Module.new
+    end
+
+    def self.start
+      @current = nil
+    end
+
+    def self.map
+      @map ||= {}
+    end
+
+    def self.finish(name)
+      ts = current::TS
+      map[name] = ts
+      const_set name.capitalize.to_sym, ts
+    end
+
+    def self.get(name)
+      map[name]
+    end
   end
 end
