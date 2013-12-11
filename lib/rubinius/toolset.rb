b0bcdb6872135b416f707e2744bd17e9005f76ed
Joe McIlvain <joe.eli.mac@gmail.com>
2013-12-10 22:12:00 -0500

    Add some minimal documentation for Rubinius::ToolSet methods

--- lib/rubinius/toolset.rb
+++ lib/rubinius/toolset.rb
@@ -2,18 +2,22 @@ require "rubinius/toolset/version"
 
 module Rubinius
   module ToolSet
+    # Access the current toolset module
     def self.current
       @current ||= Module.new
     end
 
+    # Start a new toolset module by clearing out the current one
     def self.start
       @current = nil
     end
 
+    # Access the Hash containing the finished toolset modules
     def self.map
       @map ||= {}
     end
 
+    # Finish the current toolset module by registering it with a name
     def self.finish(name)
       ts = current::TS
       ts.const_set :ToolSet, ts
@@ -21,6 +25,7 @@ module Rubinius
       const_set name.to_s.capitalize.to_sym, ts
     end
 
+    # Get a finished toolset module by name
     def self.get(name)
       map[name]
     end
