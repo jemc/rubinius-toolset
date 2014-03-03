8fe1bf6a8b0b38a29f7eabc6e2e45479eb9e7370
Brian Shirai <brixen@gmail.com>
2014-03-02 22:22:50 -0600

    Initial work on a better API.

--- lib/rubinius/toolset.rb
+++ lib/rubinius/toolset.rb
@@ -1,7 +1,7 @@
 require "rubinius/toolset/version"
 
 module Rubinius
-  module ToolSet
+  module ToolSets
     # Returns the module enclosing the current toolset.
     def self.current
       @current ||= Module.new
@@ -12,35 +12,13 @@ module Rubinius
       @current = nil
     end
 
-    # Access the Hash containing the finished toolset modules
-    def self.map
-      @map ||= {}
-    end
-
-    # Finish the current toolset module by registering it with a name
-    def self.finish(name)
-      ts = current::TS
-      ts.const_set :ToolSet, ts
-      map[name] = ts
-      const_set name.to_s.capitalize.to_sym, ts
-    end
-
-    # Get a finished toolset module by name
-    def self.get(name)
-      map[name]
-    end
-
-    # Set the current toolset module as a constant on Rubinius::ToolSet
+    # Set the current toolset module as a constant on Rubinius::ToolSets
     def self.attach(name)
+      current.const_set :ToolSet, current
       return unless name
-
-      map[name] = current
 
       name = name.to_s.split("_").map { |x| x.capitalize }.join
       const_set name, current
-
-      current.const_set :TS, current
-      current::TS.const_set :ToolSet, current
     end
 
     # Create a new toolset, optionally with a name. The module enclosing the
--- spec/toolset_spec.rb
+++ spec/toolset_spec.rb
@@ -1,63 +1,41 @@
-describe "Rubinius::ToolSet.current" do
+describe "Rubinius::ToolSets.current" do
   it "returns a Module" do
-    Rubinius::ToolSet.current.should be_an_instance_of(Module)
+    Rubinius::ToolSets.create do
+      Rubinius::ToolSets.current.should be_an_instance_of(Module)
+    end
   end
 end
 
-describe "Rubinius::ToolSet.finish" do
-  before do
-    Rubinius::ToolSet.start
-    module Rubinius::ToolSet.current::TS; end
-  end
-
-  it "adds name to the ToolSet map" do
-    Rubinius::ToolSet.finish :testing
-    Rubinius::ToolSet.map.keys.should include(:testing)
-  end
-
-  it "sets a constant on ToolSet with the capitalized name" do
-    Rubinius::ToolSet.finish :name
-    Rubinius::ToolSet::Name.should be_an_instance_of(Module)
-  end
-end
-
-describe "Rubinius::ToolSet.create" do
+describe "Rubinius::ToolSets.create" do
   it "it resets $LOADED_FEATURES while running the block" do
-    Rubinius::ToolSet.create do
+    Rubinius::ToolSets.create do
       $LOADED_FEATURES.should == []
     end
   end
 
   it "yields an anonymous Module to the block if no name is given" do
-    Rubinius::ToolSet.create do |m|
+    Rubinius::ToolSets.create do |m|
       m.should be_an_instance_of(Module)
       m.name.should be_nil
     end
   end
 
   it "sets .current to the module that is yielded to the block" do
-    Rubinius::ToolSet.create do |m|
-      m.should equal(Rubinius::ToolSet.current)
+    Rubinius::ToolSets.create do |m|
+      m.should equal(Rubinius::ToolSets.current)
     end
   end
 
   it "accepts an optional name for the toolset module" do
-    Rubinius::ToolSet.create :a_tool_set do |m|
-      m.name.should == "Rubinius::ToolSet::AToolSet"
+    Rubinius::ToolSets.create :a_tool_set do |m|
+      m.name.should == "Rubinius::ToolSets::AToolSet"
     end
   end
 
-  it "creates a temporary module named 'TS' on the current toolset module" do
-    Rubinius::ToolSet.create :spec do |m|
-      m.should equal(m::TS)
-      m::TS.name.should == "Rubinius::ToolSet::Spec"
-    end
-  end
-
-  it "creates a ToolSet module under the TS module" do
-    Rubinius::ToolSet.create :spec do |m|
-      m::TS.should equal(m::TS::ToolSet)
-      m::TS::ToolSet.name.should == "Rubinius::ToolSet::Spec"
+  it "sets the 'ToolSet' constant on the yielded module to refer to itself" do
+    Rubinius::ToolSets.create :spec do |m|
+      m.should equal(m::ToolSet)
+      m::ToolSet.name.should == "Rubinius::ToolSets::Spec"
     end
   end
 end
